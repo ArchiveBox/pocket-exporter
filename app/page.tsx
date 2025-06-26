c807657d077b8e1e2c3307f4b44850ba1f0004b1
Nick Sweeting <git@sweeting.me>
2025-06-25 17:07:37 -0700

    show archive.org link in ui

--- app/page.tsx
+++ app/page.tsx
@@ -1049,11 +1049,26 @@ export default function PocketExportApp() {
                           href={`https://getpocket.com/read/${article.item.readerSlug || article.savedId}`}
                           rel="noopener noreferrer"
                           className="text-xs text-gray-400 hover:text-red-400 transition-colors"
+                          title="View article in Pocket"
                         >
                           <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" className="icon" style={{height: '24px', display: 'inline-block'}}><path fillRule="evenodd" d="M1 4a2 2 0 0 1 2-2h18a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2zm2 0v2h18V4z" clipRule="evenodd"></path><path fillRule="evenodd" d="M3 8a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v10a4 4 0 0 1-4 4H7a4 4 0 0 1-4-4zm2 10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8H5z" clipRule="evenodd"></path><path fillRule="evenodd" d="M15.707 11.293a1 1 0 0 1 0 1.414l-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 1 1 1.414-1.414L11 14.586l3.293-3.293a1 1 0 0 1 1.414 0" clipRule="evenodd"></path></svg> 
                           &nbsp;&nbsp;
                           Pocket ID: #<code>{article.savedId}</code>
                         </a>
+                        {article.archivedotorg_url && (
+                          <>
+                            &nbsp;&nbsp;
+                            <a
+                              href={article.archivedotorg_url}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              className="text-xs hover:text-blue-600 transition-colors"
+                              title="View on Archive.org"
+                            >
+                              🏛️
+                            </a>
+                          </>
+                        )}
                       </div>
                     </CardContent>
                   </Card>
